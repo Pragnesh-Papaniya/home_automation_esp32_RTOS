d34e176b073876c9c1603b421440bb5c76ec2596
Pragnesh-Papaniya <pragneshpapaniya007@gmail.com>
2024-03-05 12:34:23 +0530

    used IR interrupt, instead of polling

--- Pragnesh_sensors/Pragnesh_sensors.ino
+++ Pragnesh_sensors/Pragnesh_sensors.ino
@@ -13,34 +13,35 @@
 #define FlamePin 2
 #define BuzzPin 23
 
-DHT dht(DhtPin,DHT11);
+DHT dht(DhtPin, DHT11);
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-void setup() {//setup task runs at priority 1
+void IRAM_ATTR FlameIsr();  // Flame Isr declaration.
+
+void setup() {  //setup task runs at priority 1
   dht.begin();
-  vTaskDelay(2000/portTICK_PERIOD_MS);
+  vTaskDelay(2000 / portTICK_PERIOD_MS);
   pinMode(FlamePin, INPUT);
   pinMode(BuzzPin, OUTPUT);
   Serial.begin(300);
   xTaskCreatePinnedToCore(Task_DHT11, "Task_DHT11", 1024, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
   xTaskCreatePinnedToCore(Task_TOF, "Task_TOF", 2024, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
-  xTaskCreatePinnedToCore(Task_Flame, "Task_Flame", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
+  attachInterrupt(FlamePin, FlameIsr, RISING);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  
 }
 
-void Task_DHT11(void *pvParameters) // This is a task.
+void Task_DHT11(void *pvParameters)  // This is a task.
 {
   (void)pvParameters;
 
-  while (! Serial) {
-    vTaskDelay(1/portTICK_PERIOD_MS);  
+  while (!Serial) {
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 
-  while (1) // A Task shall never return or exit.
+  while (1)  // A Task shall never return or exit.
   {
     float temp = dht.readTemperature();
     float humidity = dht.readHumidity();
@@ -50,53 +51,47 @@ void Task_DHT11(void *pvParameters) // This is a task.
     Serial.print("humidty: ");
     Serial.print(humidity);
     Serial.println("%");
-    vTaskDelay(2000/portTICK_PERIOD_MS);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
 }
 
-void Task_TOF(void *pvParameters) // This is a task.
+void Task_TOF(void *pvParameters)  // This is a task.
 {
   (void)pvParameters;
 
   // wait until serial port opens for native USB devices
-  while (! Serial) {
-    vTaskDelay(1/portTICK_PERIOD_MS);  
+  while (!Serial) {
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
+    while (1)
+      ;
   }
 
-  while (1) // A Task shall never return or exit.
+  while (1)  // A Task shall never return or exit.
   {
     VL53L0X_RangingMeasurementData_t measure;
 
     // Serial.print("Reading a measurement... ");
-    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+    lox.rangingTest(&measure, false);  // pass in 'true' to get debug data printout!
 
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-      Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+      Serial.print("Distance (mm): ");
+      Serial.println(measure.RangeMilliMeter);
     } else {
       Serial.println(" out of range ");
     }
-    vTaskDelay(100/portTICK_PERIOD_MS);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-void Task_Flame(void *pvParameters) // This is a task.
+void IRAM_ATTR FlameIsr()  // flame isr that is to be executed
 {
-  
-  (void)pvParameters;
-  
-  while (1) // A Task shall never return or exit.
-  {
-    if((digitalRead(FlamePin))==1){
-      digitalWrite(BuzzPin,HIGH);
-    }
-    else{
-      digitalWrite(BuzzPin,LOW);
-    }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+  if ((digitalRead(FlamePin)) == 1) {
+    digitalWrite(BuzzPin, HIGH);
+  } else {
+    digitalWrite(BuzzPin, LOW);
   }
 }
