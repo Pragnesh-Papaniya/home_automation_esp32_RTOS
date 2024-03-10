c7391f1622df9470aff63f76f45249706fa4165b
vpatel1609 <vedantipatel2003@gmail.com>
2024-03-10 23:16:31 +0530

    vTask Created
    vTask Created for soil moisture and gas sensor and an ISSR for Pir sensor

--- Vedanti_sensors/vedanti_sensors.ino
+++ Vedanti_sensors/vedanti_sensors.ino
@@ -1,19 +1,40 @@
-//Gas Sensor with (active) piezoelectric Buzzer
-//4 pins: VCC,GND,D0,A0
+//Use only core 1 
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else 
+static const BaseType_t app_cpu = 1;
+#endif
+
+//Pins
+static const int soilMoisture = 28;
+static const int pirSensor = 27;
+static const int gasSensor = 26;
+static const int buzzer = 2;
 
 #define timeSeconds 10
 
-//Set Pins
-const int pirSensor = 27;
-const int gasSensor = 26;  //Using Digiatal Pin
-const int buzzer = 2;
-
 //Timer
 long currentTime = millis();
-long lastTrigger = 
-int sensorValue = 0;
+long lastTrigger = 0;             //holds time when PIR detects motion
+boolean startTimer = false;       //starts timer when motion is detected
 
-//IRAM_ATTR for PIR sensor
+//Our task: measure soil moisture
+void measureMoisture(void *parameter){
+  while(1){
+    Serial.println("Moisture:");
+    Serial.print(analogRead(soilMoisture));
+  }
+}
+
+//Our task: measure soil humidity
+void gasDetect(void *parameter){
+  while(1){
+    Serial.println("Gas:");
+    Serial.print(analogRead(gasSensor));
+  }
+}
+
+//IRAM_ATTR: to detect movement
 void detectMovement(){
   digitalWrite(buzzer, HIGH);
   startTimer = true;
@@ -23,17 +44,38 @@ void detectMovement(){
 }
 
 void setup() {
+  pinMode(soilMoisture,INPUT);
   pinMode(gasSensor, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(gasSensor), detectGas, FALLING);
+  pinMode(pirSensor, INPUT_PULLUP); 
   
   pinMode(buzzer, OUTPUT);
   digitalWrite(buzzer, LOW);
+
+  xTaskCreatePinnedToCore(
+    measureMoisture,
+    "Measure soil moisture",
+    1024,
+    NULL,
+    1,
+    NULL,
+    app_cpu);
+
+    xTaskCreatePinnedToCore(
+    gasDetect,
+    "Detect Gases",
+    1024,
+    NULL,
+    1,
+    NULL,
+    app_cpu);
+
+  attachInterrupt(digitalPinToInterrupt(pirSensor), detectMovement, RISING);   //(pin,function,mode)
 
   Serial.begin(115200);
 }
 
 void loop() {
-  currentTime = millis();
+   currentTime = millis();
 
   if((startTimer)&&(currentTime - lastTrigger>(timeSeconds*1000))){
     digitalWrite(buzzer, LOW);
@@ -41,10 +83,4 @@ void loop() {
 
     startTimer = false;
   }
-
-  //Gas sensor
-  sensorValue = analogRead(gasSensor);
-  Serial.println(sensorValue);
-  
-  delay(500);
 }
